45bf545f6b8be831850f579bc7cdcf2a9b0a2244
Allison So <aso43@uwo.ca>
2025-03-11 20:44:31 -0400

    updates for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
-# Use an official PHP + Apache image
 FROM php:8.1-apache
 
-# Install necessary PHP extensions for MySQL
 RUN docker-php-ext-install mysqli pdo pdo_mysql
 
-# Copy project files to Apache web root
 COPY . /var/www/html/
 
-# Set correct permissions
 RUN chown -R www-data:www-data /var/www/html
 
-# Expose port 80
 EXPOSE 80
 
-# Start Apache
 CMD ["apache2-foreground"]
--- includes/connectdb.php
+++ includes/connectdb.php
@@ -1,12 +1,20 @@
 <?php
-$dbConfig = require_once __DIR__ . '/../config/database.php';
+// $dbConfig = require_once __DIR__ . '/../config/database.php';
 
-$connection = mysqli_connect(
-    $dbConfig['host'], 
-    $dbConfig['username'], 
-    $dbConfig['password'], 
-    $dbConfig['database'], 
-    $dbConfig['port']);
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$port=getenv('DB_PORT');
+
+$connection = mysqli_connect($servername, $username, $password, $dbname, $port);
+    // $dbConfig['host'],
+    // $dbConfig['username'],
+    // $dbConfig['password'],
+    // $dbConfig['database'],
+    // $dbConfig['port']);
+
+
 
 if (mysqli_connect_errno()){
     die("Database connection failed: ".mysqli_connect_error()."(".mysqli_connect_errno().")");
